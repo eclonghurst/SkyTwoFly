d167420d6accfc2504b1f8fcd2ab1deec8d7a9b5
br-watson <bradleyrwatson@hotmail.com>
2023-09-25 17:58:24 +0100

    Started implementing FlightDTO and FlightService. UNFINISHED

--- src/main/java/com/sky/GetYourWay/dtos/FlightDTO.java
+++ src/main/java/com/sky/GetYourWay/dtos/FlightDTO.java
@@ -1,4 +1,113 @@
 package com.sky.GetYourWay.dtos;
 
+import java.util.List;
+
 public class FlightDTO {
+    private List<String> flightNos;
+    private String flyFrom;
+    private String flyTo;
+    private String cityFrom;
+    private String cityTo;
+    private Integer duration;
+    private String localDeparture;
+    private String localArrival;
+    private int availableSeats;
+    private String bookingToken;
+
+    public FlightDTO() {
+        super();
+    }
+
+    public FlightDTO(List<String> flightNos, String flyFrom, String flyTo, String cityFrom, String cityTo, Integer duration, String localDeparture, String localArrival, int availableSeats, String bookingToken) {
+        this.flightNos = flightNos;
+        this.flyFrom = flyFrom;
+        this.flyTo = flyTo;
+        this.cityFrom = cityFrom;
+        this.cityTo = cityTo;
+        this.duration = duration;
+        this.localDeparture = localDeparture;
+        this.localArrival = localArrival;
+        this.availableSeats = availableSeats;
+        this.bookingToken = bookingToken;
+    }
+
+    public String getBookingToken() {
+        return bookingToken;
+    }
+
+    public void setBookingToken(String bookingToken) {
+        this.bookingToken = bookingToken;
+    }
+
+    public List<String> getFlightNos() {
+        return flightNos;
+    }
+
+    public void setFlightNos(List<String> flightNos) {
+        this.flightNos = flightNos;
+    }
+
+    public String getFlyFrom() {
+        return flyFrom;
+    }
+
+    public void setFlyFrom(String flyFrom) {
+        this.flyFrom = flyFrom;
+    }
+
+    public String getFlyTo() {
+        return flyTo;
+    }
+
+    public void setFlyTo(String flyTo) {
+        this.flyTo = flyTo;
+    }
+
+    public String getCityFrom() {
+        return cityFrom;
+    }
+
+    public void setCityFrom(String cityFrom) {
+        this.cityFrom = cityFrom;
+    }
+
+    public String getCityTo() {
+        return cityTo;
+    }
+
+    public void setCityTo(String cityTo) {
+        this.cityTo = cityTo;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getLocalDeparture() {
+        return localDeparture;
+    }
+
+    public void setLocalDeparture(String localDeparture) {
+        this.localDeparture = localDeparture;
+    }
+
+    public String getLocalArrival() {
+        return localArrival;
+    }
+
+    public void setLocalArrival(String localArrival) {
+        this.localArrival = localArrival;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
 }
---
+++ src/main/java/com/sky/GetYourWay/services/FlightService.java
@@ -0,0 +1,61 @@
+package com.sky.GetYourWay.services;
+
+
+import com.sky.GetYourWay.domain.APIObjects.Datum;
+import com.sky.GetYourWay.domain.APIObjects.FlightAPIResponse;
+import com.sky.GetYourWay.dtos.FlightDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FlightService {
+
+    private final WebClient apiClient;
+
+    public FlightService(WebClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public List<FlightDTO> getFlightsOneWay(String outboundLocation, String returnLocation, String outboundDate, String returnDate) {
+
+        FlightAPIResponse apiResponse = apiClient.get()
+                .uri("https://api.tequila.kiwi.com/v2/search?fly_from=" + outboundLocation + "&fly_to="+ returnLocation +"&date_from=" + outboundDate + "&date_to=" + returnDate)
+                .header("apikey", "BVXX0vJu26lyp1RFal0rdGumfTLiUmr2")
+                .retrieve()
+                .bodyToMono(FlightAPIResponse.class)
+                .block();
+
+        List<FlightDTO> flights = new ArrayList<>();
+
+        for (int i = 0; i < apiResponse.getData().size(); i++) {
+            Datum current = apiResponse.getData().get(i);
+            List<String> flightNos = new ArrayList<>();
+
+            for (int j = 0; j < current.getRoute().size(); j++)
+                flightNos.add(current.getRoute().get(j).getFlightNo().toString());
+
+
+            FlightDTO flight = new FlightDTO(flightNos, current.getFlyFrom(), current.getFlyTo(), current.getCityFrom(), current.getCityTo(), current.getDuration().getTotal(), current.getLocalDeparture(), current.getLocalArrival(), current.getAvailability().getSeats(), current.getBookingToken());
+            flights.add(flight);
+        }
+        return flights;
+    }
+
+    public List<FlightDTO> getFlightsOneWayFilters(){
+        return null;
+    }
+
+    public List<FlightDTO> getFlightsReturn(){
+        return null;
+    }
+
+    public List<FlightDTO> getFlightsReturnFilters(){
+        return null;
+    }
+
+
+
+}
